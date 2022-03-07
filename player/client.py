15741d33447006d2582ba3c325c9dfb44097550f
Berger <Bergeredvard6@gmail.com>
2022-03-07 14:52:26 +0100

    Fungerende komunikasjon mellom server og bruker

--- player/client.py
+++ player/client.py
@@ -1,12 +1,21 @@
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
+HOST = "127.0.0.1"
+Local = "localhost"# The server's hostname or IP address
 PORT = 65432  # The port used by the server
 address = (HOST, PORT)
-from socket import socket
+import socket
 
-
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((Local, PORT))
+    s.sendall(b"Hello, world")
+    while True:
+        inp = input(">")
+        s.sendall(inp.encode())
+    data = s.recv(2048)
+'''
 sock = socket()
 sock.connect(address)
 message = "Hei"
 sock.send(message.encode())
+'''
--- server/server.py
+++ server/server.py
@@ -12,23 +12,28 @@ lsock.bind((HOST, PORT))
 lsock.listen()
 print(f"Listening on {(HOST, PORT)}")
 lsock.setblocking(False)
-sel.register(lsock, selectors.EVENT_READ, data=None)
+sel.register(lsock, selectors.EVENT_READ, True)
 #AF_INET - use ipv4 addresses
 #SOCK_STREAM - use tcp
 
+
 def accept(sock):
+    print("DEN KJØRER")
+
+    print(sock)
     conn, addr = sock.accept()  # Should be ready to read
     print(f"Accepted connection from {addr}")
     conn.setblocking(False)
     sel.register(conn, EVENT_READ)
 
-def read(conn):
 
+def read(conn):
     recv_data = conn.recv(2048)  # Should be ready to read
     if recv_data:
         print(recv_data.decode())
     else:
-        print(f"Closing connection to {recv_data.addr}")
+        #print(f"Closing connection to {recv_data.addr}")
+        print("Closing connection")
         sel.unregister(recv_data)
         recv_data.close()
 
@@ -38,10 +43,10 @@ try:
     while True:
         events = sel.select(timeout=None)
         for key, mask in events:
-            if key.data is None:
+            if key.data:
                 accept(key.fileobj)
             else:
-                read(key, mask)
+                read(key.fileobj)
 except KeyboardInterrupt:
     print("Caught keyboard interrupt, exiting")
 finally:
