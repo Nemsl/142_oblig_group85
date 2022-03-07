c6a4533370e66f85e5536fc47d5f37bbf38740bd
Berger <Bergeredvard6@gmail.com>
2022-03-07 11:49:24 +0100

    Two python scripts communicating with sockets

--- player/client.py
+++ player/client.py
@@ -3,10 +3,8 @@ import socket
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 address = (HOST, PORT)
-import sys
 from socket import socket
-import selectors
-import types
+
 
 sock = socket()
 sock.connect(address)
